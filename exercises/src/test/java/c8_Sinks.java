f93304ed92697d4f2d86ad7f921d5a05e0e0a795
asmisloff <a.smisloff@gmail.com>
2026-01-31 09:56:54 +0300

    c8_2

--- exercises/src/test/java/c8_Sinks.java
+++ exercises/src/test/java/c8_Sinks.java
@@ -196,7 +196,16 @@ public class c8_Sinks extends SinksBase {
 
         for (int i = 1; i <= 50; i++) {
             int finalI = i;
-            new Thread(() -> sink.tryEmitNext(finalI)).start();
+            new Thread(() ->  {
+                while (sink.tryEmitNext(finalI) == Sinks.EmitResult.FAIL_NON_SERIALIZED) {
+                    try {
+                        Thread.sleep(Duration.ofMillis(10).toMillis());
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            })
+                .start();
         }
 
         //don't change code below
